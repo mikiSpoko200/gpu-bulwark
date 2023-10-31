4622ec2f145d4d7dadcd93d8ff155ff2d7e7535f
mikiSpoko200 <328690@uwr.edu.pl>
2023-10-31 10:42:30 +0100

    Refactor to using gl_call and formatting

--- src/object/buffer.rs
+++ src/object/buffer.rs
@@ -1,16 +1,18 @@
-use std::marker::PhantomData;
-use gl::types::{GLenum, GLuint};
-use crate::error;
-use crate::prelude::Const;
 use super::prelude::*;
+use super::resource::{Bindable, Resource};
+use crate::prelude::Const;
 use crate::targets::buffer;
-use super::resource::{Resource, Bindable};
+use crate::{error, gl_call};
+use gl::types::{GLenum, GLuint};
+use std::marker::PhantomData;
 
-pub struct Buffer<Target> where Target: buffer::Target {
+pub struct Buffer<Target>
+where
+    Target: buffer::Target,
+{
     base: Object,
     _target_phantom: PhantomData<Target>,
 }
-
 
 pub trait Usage: Const<GLenum> {}
 
@@ -27,102 +29,99 @@ pub struct Read;
 pub struct Copy;
 
 // impls for Stream access frequency
-impl Const<GLenum> for (Stream, Draw) { const VALUE: GLenum = gl::STREAM_DRAW; }
-
-impl Usage for (Stream, Draw) {}
-
-impl Const<GLenum> for (Stream, Read) { const VALUE: GLenum = gl::STREAM_READ; }
-
-impl Usage for (Stream, Read) {}
-
-impl Const<GLenum> for (Stream, Copy) { const VALUE: GLenum = gl::STREAM_COPY; }
-
-impl Usage for (Stream, Copy) {}
-
+crate::impl_const_super_trait!(Usage for (Stream, Draw), gl::STREAM_DRAW);
+crate::impl_const_super_trait!(Usage for (Stream, Read), gl::STREAM_READ);
+crate::impl_const_super_trait!(Usage for (Stream, Copy), gl::STREAM_COPY);
 
 // impls for Static access frequency
-impl Const<GLenum> for (Static, Draw) { const VALUE: GLenum = gl::STATIC_DRAW; }
-
-impl Usage for (Static, Draw) {}
-
-impl Const<GLenum> for (Static, Read) { const VALUE: GLenum = gl::STATIC_READ; }
-
-impl Usage for (Static, Read) {}
-
-impl Const<GLenum> for (Static, Copy) { const VALUE: GLenum = gl::STATIC_COPY; }
-
-impl Usage for (Static, Copy) {}
+crate::impl_const_super_trait!(Usage for (Static, Draw), gl::STATIC_DRAW);
+crate::impl_const_super_trait!(Usage for (Static, Read), gl::STATIC_READ);
+crate::impl_const_super_trait!(Usage for (Static, Copy), gl::STATIC_COPY);
 
 // impls for Dynamic access frequency
-impl Const<GLenum> for (Dynamic, Draw) { const VALUE: GLenum = gl::DYNAMIC_DRAW; }
+crate::impl_const_super_trait!(Usage for (Dynamic, Draw), gl::DYNAMIC_DRAW);
+crate::impl_const_super_trait!(Usage for (Dynamic, Read), gl::DYNAMIC_READ);
+crate::impl_const_super_trait!(Usage for (Dynamic, Copy), gl::DYNAMIC_COPY);
 
-impl Usage for (Dynamic, Draw) {}
-
-impl Const<GLenum> for (Dynamic, Read) { const VALUE: GLenum = gl::DYNAMIC_READ; }
-
-impl Usage for (Dynamic, Read) {}
-
-impl Const<GLenum> for (Dynamic, Copy) { const VALUE: GLenum = gl::DYNAMIC_COPY; }
-
-impl Usage for (Dynamic, Copy) {}
-
-impl<Target> Buffer<Target> where Target: buffer::Target {
+impl<Target> Buffer<Target>
+where
+    Target: buffer::Target,
+{
     fn data<U>(&self, data: &[u8])
-        where
-            U: Usage
+    where
+        U: Usage,
     {
         // TODO: error handling
-        unsafe {
-            gl::BufferData(
-                Target::BIND_TARGET,
-                data.len() as _,
-                data.as_ptr() as _,
-                U::VALUE
-            );
+        gl_call! { #[panic] unsafe {
+                gl::BufferData(
+                    Target::BIND_TARGET,
+                    data.len() as _,
+                    data.as_ptr() as _,
+                    U::VALUE,
+                );
+            }
         }
     }
 }
 
-impl<Target> From<Object> for Buffer<Target> where Target: buffer::Target {
+impl<Target> From<Object> for Buffer<Target>
+where
+    Target: buffer::Target,
+{
     fn from(base: Object) -> Self {
-        Self { base, _target_phantom: Default::default() }
+        Self {
+            base,
+            _target_phantom: Default::default(),
+        }
     }
 }
 
-impl<Target> Into<Object> for Buffer<Target> where Target: buffer::Target {
+impl<Target> Into<Object> for Buffer<Target>
+where
+    Target: buffer::Target,
+{
     fn into(self) -> Object {
         let Self { base, .. } = self;
         base
     }
 }
 
-impl<Target> Bindable for Buffer<Target> where Target: buffer::Target {
+impl<Target> Bindable for Buffer<Target>
+where
+    Target: buffer::Target,
+{
     fn bind(&self) {
-        unsafe {
-            gl::BindBuffer(Target::BIND_TARGET, self.base.0)
+        gl_call! {
+            #[panic]
+            unsafe { gl::BindBuffer(Target::BIND_TARGET, self.base.0) }
         }
     }
 
     fn unbind(&self) {
-        unsafe {
-            gl::BindBuffer(Target::BIND_TARGET, 0);
+        gl_call! {
+            #[panic]
+            unsafe { gl::BindBuffer(Target::BIND_TARGET, 0) }
         }
     }
 }
 
-impl<Target> Resource for Buffer<Target> where Target: buffer::Target {
+impl<Target> Resource for Buffer<Target>
+where
+    Target: buffer::Target,
+{
     type Ok = ();
 
     fn initialize(names: &mut [GLuint]) -> error::Result<Self::Ok> {
-        unsafe {
-            gl::CreateBuffers(names.len() as _, names.as_mut_ptr());
+        gl_call! {
+            #[propagate]
+            unsafe { gl::CreateBuffers(names.len() as _, names.as_mut_ptr()); }
         }
-        error::Error::poll_queue().map_or(Ok(()), Err)
     }
 
     fn free(names: &[GLuint]) -> error::Result<Self::Ok> {
-        unsafe {
-            gl::DeleteBuffers(names.len() as _, names.as_ptr())
+        gl_call! {
+            #[propagate]
+            unsafe { gl::DeleteBuffers(names.len() as _, names.as_ptr()); }
         }
-        error::Error::poll_queue().map_or(Ok(()), Err)
-    } }
+    }
+}
--- src/object/shader.rs
+++ src/object/shader.rs
@@ -1,59 +1,153 @@
-use std::marker::PhantomData;
-use gl::types::GLenum;
-use crate::object::resource::Resource;
 use super::prelude::*;
+use crate::object::resource::Resource;
 use crate::prelude::*;
+use crate::{gl_call, impl_const_super_trait};
+use gl::types::GLenum;
+use std::marker::PhantomData;
 
-pub trait Stage: Const<GLenum> { }
+use thiserror;
 
+pub trait Stage: Const<GLenum> {}
+
+/// Zero-sized struct that represents Vertex Shader stage.
 pub struct Vertex;
+
 pub mod tesselation {
+    /// Zero-sized struct that represents Tesselation Control Shader stage.
     pub struct Control;
+
+    /// Zero-sized struct that represents Tesselation Evaluation Shader stage.
     pub struct Evaluation;
 }
+
+/// Zero-sized struct that represents Geometry Shader stage.
 pub struct Geometry;
+
+/// Zero-sized struct that represents Fragment Shader stage.
 pub struct Fragment;
+
+/// Zero-sized struct that represents Compute Shader stage.
 pub struct Compute;
 
-
-impl Const<GLenum> for Vertex { const VALUE: GLenum = gl::VERTEX_SHADER; }
-impl Stage for Vertex { }
-
-impl Const<GLenum> for tesselation::Control { const VALUE: GLenum = gl::TESS_CONTROL_SHADER; }
-impl Stage for tesselation::Control { }
-
-impl Const<GLenum> for tesselation::Evaluation { const VALUE: GLenum = gl::TESS_EVALUATION_SHADER; }
-impl Stage for tesselation::Evaluation { }
-
-impl Const<GLenum> for Geometry { const VALUE: GLenum = gl::GEOMETRY_SHADER; }
-impl Stage for Geometry { }
-
-impl Const<GLenum> for Fragment { const VALUE: GLenum = gl::FRAGMENT_SHADER; }
-impl Stage for Fragment { }
-
-impl Const<GLenum> for Compute { const VALUE: GLenum = gl::COMPUTE_SHADER; }
-impl Stage for Compute { }
+impl_const_super_trait!(Stage for Vertex, gl::VERTEX_SHADER);
+impl_const_super_trait!(Stage for tesselation::Control, gl::TESS_CONTROL_SHADER);
+impl_const_super_trait!(Stage for tesselation::Evaluation, gl::TESS_EVALUATION_SHADER);
+impl_const_super_trait!(Stage for Geometry, gl::GEOMETRY_SHADER);
+impl_const_super_trait!(Stage for Fragment, gl::FRAGMENT_SHADER);
+impl_const_super_trait!(Stage for Compute, gl::COMPUTE_SHADER);
 
 
-pub struct Shader<S> where S: Stage {
+pub trait  
+
+
+pub struct Shader<S, In, Out>
+where
+    S: Stage,
+{
     base: Object,
-    _stage_phantom: PhantomData<S>
+    _stage_phantom: PhantomData<S>,
+    _in_phantom: PhantomData<In>,
+    _out_phantom: PhantomData<Out>,
 }
 
-impl<S> Into<Object> for Shader<S> where S: Stage {
+#[repr(u32)]
+pub enum QueryParam {
+    ShaderType = gl::SHADER_TYPE,
+    DeleteStatus = gl::DELETE_STATUS,
+    CompileStatus = gl::COMPILE_STATUS,
+    InfoLogLength = gl::INFO_LOG_LENGTH,
+    ShaderSourceLength = gl::SHADER_SOURCE_LENGTH,
+}
+
+#[derive(thiserror::Error, Debug)]
+#[error("shader compilation failed {msg}")]
+pub struct CompilationError {
+    msg: String,
+}
+
+impl<S, In, Out> Shader<S, In, Out>
+where
+    S: Stage,
+{
+    pub unsafe fn source_from_raw(&mut self, source: &[*const i8]) {
+        gl_call! {
+            #[panic]
+            unsafe {
+                gl::ShaderSource(self.base.0, 1, source.as_ptr(), std::ptr::null());
+            }
+        }
+    }
+
+    /// Create new shader from source
+    pub fn source(&mut self, source: &[&str]) -> Result<Self, CompilationError> {
+
+
+
+    }
+
+    pub fn query(&self, param: QueryParam, output: &mut i32) {
+        gl_call! {
+            #[panic]
+            unsafe {
+                gl::GetShaderiv(self.base.0, param as _, output);
+            }
+        }
+    }
+
+    pub fn info_log(&self) -> Option<String> {
+        let mut log_size = 0;
+        self.query(QueryParam::InfoLogLength, &mut log_size);
+        (log_size > 0).then(|| {
+            let mut buffer: Vec<u8> = Vec::with_capacity(log_size as _);
+            let mut actual_length = 0;
+            gl_call! {
+                #[panic]
+                // SAFETY: All values passed are valid
+                // todo: notes on error situations
+                unsafe {
+                    gl::GetShaderInfoLog(
+                        self.base.0,
+                        buffer.capacity() as _,
+                        &mut actual_length as *mut _,
+                        buffer.as_mut_ptr() as _
+                    )
+                }
+            }
+            // GetShaderInfoLog does not account for null terminator in returned length.
+            // SAFETY: nothing will panic here so it's safe to set length.
+            unsafe { buffer.set_len((actual_length + 1) as _); }
+            // SAFETY: todo will shader compiler should emmit valid ascii?
+            unsafe { String::from_utf8_unchecked(buffer) }
+        })
+    }
+}
+
+impl<S> Into<Object> for Shader<S>
+where
+    S: Stage,
+{
     fn into(self) -> Object {
         let Self { base, .. } = self;
         base
     }
 }
 
-impl<S> From<Object> for Shader<S> where S: Stage {
+impl<S> From<Object> for Shader<S>
+where
+    S: Stage,
+{
     fn from(base: Object) -> Self {
-        Self { base, _stage_phantom: Default::default() }
+        Self {
+            base,
+            _stage_phantom: Default::default(),
+        }
     }
 }
 
-impl<S> Resource for Shader<S> where S: Stage {
+impl<S> Resource for Shader<S>
+where
+    S: Stage,
+{
     type Ok = ();
 
     fn initialize(names: &mut [Name]) -> crate::error::Result<Self::Ok> {
@@ -75,19 +169,23 @@ impl<S> Resource for Shader<S> where S: Stage {
     }
 }
 
-
-impl<S> Shader<S> where S: Stage {
-    pub fn inputs() { }
-    pub fn outputs() { }
+impl<S> Shader<S>
+where
+    S: Stage,
+{
+    pub fn inputs() {}
+    pub fn outputs() {}
 }
-
 
 pub mod program {
     use crate::object::prelude::Object;
-    use crate::object::shader::{Compute, Fragment, Geometry, Shader, Stage, tesselation, Vertex};
+    use crate::object::shader::{tesselation, Compute, Fragment, Geometry, Shader, Stage, Vertex};
 
     // Sealed trait
-    trait Attach<S> where S: Stage {
+    trait Attach<S>
+    where
+        S: Stage,
+    {
         fn attach(&mut self, shader: Shader<S>);
     }
 
@@ -137,7 +235,7 @@ pub mod program {
         }
     }
 
-    pub struct ProgramConfiguration { }
+    pub struct ProgramConfiguration {}
 
     impl Default for ProgramConfiguration {
         fn default() -> Self {
@@ -149,13 +247,15 @@ pub mod program {
         base: Object,
         config: ProgramConfiguration,
         vertex: Shader<Vertex>,
-        tesselation: Option<(Shader<tesselation::Control>, Shader<tesselation::Evaluation>)>,
+        tesselation: Option<(
+            Shader<tesselation::Control>,
+            Shader<tesselation::Evaluation>,
+        )>,
         geometry: Option<Shader<Geometry>>,
         fragment: Shader<Fragment>,
         compute: Option<Shader<Compute>>,
     }
 }
-
 
 pub struct Compiler;
 
