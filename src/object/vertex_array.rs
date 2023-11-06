6dfbf7a09b502121e8b88e0a311b37c3e6bd7e34
Mikołaj Depta <328690@uwr.edu.pl>
2023-11-06 23:51:15 +0100

    Add object/vertex_array module draft
    Initial draft of the API

---
+++ src/object/vertex_array.rs
@@ -0,0 +1,13 @@
+
+
+
+pub struct VertexArray<Attributes> {
+    inputs: Inputs
+}
+
+pub fn draw_arrays<VaoInputs, ProgramInputs>(program: (), vertex_array: VertexArray<VaoInputs>)
+where
+    
+{
+    
+}
