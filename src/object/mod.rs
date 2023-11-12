86d104cb3336426ab1a332750bc1be787eeaca41
mikiSpoko200 <328690@uwr.edu.pl>
2023-11-12 17:02:34 +0100

    Refactor imports, add test sample

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,6 +1,28 @@
+#![allow(unused)]
+
+use crate::object::resource::Handle;
+use crate::targets as target;
+
 mod buffer;
 mod prelude;
 mod resource;
 mod shader;
-mod program;
+pub mod program;
 mod vertex_array;
+
+
+use target::buffer::format;
+
+fn make<Data>() -> Handle<buffer::Buffer<target::buffer::Array, Data>>
+    where
+        (target::buffer::Array, Data): format::Valid
+{
+    Handle::new()
+}
+
+fn test() {
+    use buffer::{Static, Draw};
+
+    let buffer = make::<[f32; 3]>();
+    buffer.data::<(Static, Draw)>(&[[1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0]]);
+}
