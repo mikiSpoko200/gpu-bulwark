79b094b9546c514eb052dd68e84313cf8e0eb2b4
mikiSpoko200 <328690@uwr.edu.pl>
2023-11-12 17:03:29 +0100

    Rename const parameters to upper case

--- src/object/program/layout.rs
+++ src/object/program/layout.rs
@@ -1,66 +1,78 @@
 //! This module provides `location` glsl attribute calculations for glsl types.
 
-use crate::glsl::types::*;
-use std::marker::PhantomData;
+use crate::glsl::types;
+use types::{Const, VecSize};
 
 /// Trait that abstracts over GLSL location count that the type uses.
 pub unsafe trait Layout {
     const LOCATION_COUNT: usize;
 }
 
+unsafe impl Layout for f32 {
+    const LOCATION_COUNT: usize = 1;
+}
+
+unsafe impl Layout for f64 {
+    const LOCATION_COUNT: usize = 1;
+}
+
+unsafe impl Layout for i32 {
+    const LOCATION_COUNT: usize = 1;
+}
+
+unsafe impl Layout for u32 {
+    const LOCATION_COUNT: usize = 1;
+}
+
+unsafe impl Layout for bool {
+    const LOCATION_COUNT: usize = 1;
+}
+
 /// Location count for glsl vecX types.
-unsafe impl<const Size: usize> Layout for Vec<f32, Size>
+unsafe impl<const SIZE: usize> Layout for types::Vec<f32, SIZE>
 where
-    Const<Size>: VecSize,
+    Const<SIZE>: VecSize,
 {
     const LOCATION_COUNT: usize = 1;
 }
 
 /// Location count for glsl ivecX types.
-unsafe impl<const Size: usize> Layout for IVec<Size>
+unsafe impl<const SIZE: usize> Layout for types::IVec<SIZE>
 where
-    Const<Size>: VecSize,
+    Const<SIZE>: VecSize,
 {
     const LOCATION_COUNT: usize = 1;
 }
 
 /// Location count for glsl uvecX types.
-unsafe impl<const Size: usize> Layout for UVec<Size>
+unsafe impl<const SIZE: usize> Layout for types::UVec<SIZE>
 where
-    Const<Size>: VecSize,
+    Const<SIZE>: VecSize,
 {
     const LOCATION_COUNT: usize = 1;
 }
 
 /// Location count for glsl bvecX types.
-unsafe impl<const Size: usize> Layout for BVec<Size>
+unsafe impl<const SIZE: usize> Layout for types::BVec<SIZE>
 where
-    Const<Size>: VecSize,
+    Const<SIZE>: VecSize,
 {
     const LOCATION_COUNT: usize = 1;
 }
 
 /// Location count for glsl dvecX types is different dvec2 take 1 location and dvec3/4 take 2.
-unsafe impl Layout for DVec<2> {
-    const LOCATION_COUNT: usize = 1;
-}
-
-/// SAFETY: double check is DVec3/4 for VS actually uses up 2 locations.
-unsafe impl Layout for DVec<3> {
-    const LOCATION_COUNT: usize = 2;
-}
-
-/// SAFETY: double check is DVec3/4 for VS actually uses up 2 locations.
-unsafe impl Layout for DVec<4> {
-    const LOCATION_COUNT: usize = 2;
-}
-
-/// Layout for an Array of `T` where `T: Layout` of size `N` is `N * <T as Layout>::LOCATION_COUNT`,
-pub struct Array<T, const N: usize>(PhantomData<T>)
+unsafe impl<const SIZE: usize> Layout for types::DVec<SIZE>
 where
-    T: Layout;
+    Const<SIZE>: VecSize
+{
+    const LOCATION_COUNT: usize = match SIZE {
+        2 => 1,
+        3 | 4 => 2,
+        _ => panic!("invalid Vec size"),
+    };
+}
 
-unsafe impl<T, const N: usize> Layout for Array<T, N>
+unsafe impl<T, const N: usize> Layout for types::Array<T, N>
 where
     T: Layout,
 {
@@ -68,11 +80,12 @@ where
 }
 
 /// Accordingly to GLSL spec matrices use the same number of locations as arrays of Row
-unsafe impl<T, const Row: usize, const Col: usize> Layout for Mat<T, Row, Col>
+unsafe impl<T, const ROW: usize, const COL: usize> Layout for types::Mat<T, ROW, COL>
 where
     T: Layout,
-    Const<Row>: VecSize,
-    Const<Col>: VecSize,
+    Const<ROW>: VecSize,
+    Const<COL>: VecSize,
+    types::Vec<T, COL>: Layout,
 {
-    const LOCATION_COUNT: usize = <Array<Vec<T, Col>, Row> as Layout>::LOCATION_COUNT;
+    const LOCATION_COUNT: usize = <types::Array<types::Vec<T, COL>, ROW> as Layout>::LOCATION_COUNT;
 }
