5dae7b63f72bcd29a30c1739eef25fd5168e34b3
mikiSpoko200 <328690@uwr.edu.pl>
2023-11-12 17:07:21 +0100

    Add rudimentary format submodule implementation

--- src/targets/buffer.rs
+++ src/targets/buffer.rs
@@ -36,3 +36,22 @@ impl_target!(buffer, ShaderStorage, SHADER_STORAGE_BUFFER);
 impl_target!(buffer, Texture, TEXTURE_BUFFER);
 impl_target!(buffer, TransformFeedback, TRANSFORM_FEEDBACK_BUFFER);
 impl_target!(buffer, Uniform, UNIFORM_BUFFER);
+
+pub mod format {
+    /// Relation of types being valid data formats for given target.
+    pub unsafe trait Valid { }
+
+    /// This exploits the 3rd rule of unconstrained type parameter exceptions
+    /// "be bound as an associated type."
+    pub trait Id {
+        type Id;
+    }
+
+    // Doing this through a blanket impl would yield two main benefits:
+    // 1. DRY - relation between Gl/Glsl would be established only once and rest would follow.
+    // 2. I forgot the second one.
+
+    unsafe impl Valid for (super::Array, [f32; 3]) { }
+    unsafe impl Valid for (super::Array, [f32; 4]) { }
+    unsafe impl Valid for (super::Array, f32) { }
+}
