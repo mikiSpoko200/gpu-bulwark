df8255df3b0b8674c9221f8794d94d47788d404b
mikiSpoko200 <328690@uwr.edu.pl>
2023-11-30 00:01:51 +0100

    Fix target rename artefacts

--- src/targets/mod.rs
+++
@@ -1,8 +0,0 @@
-/// Definitions of opengl bind targets
-pub mod buffer;
-pub mod prelude;
-
-/// Common behaviour amongst all object specific targets
-pub(self) unsafe trait BaseTarget {
-    const BIND_TARGET: gl::types::GLenum;
-}
