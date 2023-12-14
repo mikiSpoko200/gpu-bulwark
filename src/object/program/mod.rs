a18e7eb62a152367349b111cb2639a4969b2c287
mikiSpoko200 <328690@uwr.edu.pl>
2023-12-14 13:43:49 +0100

    Finalize Program API

--- src/object/program/mod.rs
+++ src/object/program/mod.rs
@@ -1,45 +1,53 @@
+pub mod attach;
+pub mod stage;
+pub mod parameters;
+
 use frunk::labelled::chars::T;
-use nonempty::{nonempty, NonEmpty};
+use gl::CompileShader;
 
 use super::prelude::Object;
-use super::resource::Allocator;
-use super::shader::{Compiled, Shader};
+use super::resource::{Allocator, self};
+use super::shader::{Compiled, Shader, Main, TargetProvider, Shared, CompilationError};
 use crate::gl_call;
+use crate::glsl;
 use crate::target::shader;
 use crate::target::shader::{tesselation, Compute, Fragment, Geometry, Vertex};
+use crate::types::Unimplemented;
 use std::marker::PhantomData;
-
-pub mod layout;
 
 pub type CompiledShader<T> = Shader<T, Compiled>;
 
-pub trait ShaderInterface {}
+pub type VertexShader = CompiledShader<Vertex>;
+pub type TesselationControlShader = CompiledShader<tesselation::Control>;
+pub type TesselationEvaluationShader = CompiledShader<tesselation::Evaluation>;
+pub type GeometryShader = CompiledShader<Geometry>;
+pub type FragmentShader = CompiledShader<Fragment>;
+pub type ComputeShader = CompiledShader<Compute>;
 
 /// Collection of shaders for given program stage with defined stage interface.
 ///
 /// It contains exactly one shaders that contains main function
-/// and arbitrary many that are there just to supply subroutines.
-struct ShaderStage<'shaders, T, Inputs, Outputs>
+/// and arbitrary many that are there just to supply shaders to link against.
+pub(crate) struct ShaderStage<'shaders, T>
 where
     T: shader::Target,
 {
-    pub shaders: NonEmpty<&'shaders CompiledShader<T>>,
-    _in_phantom: PhantomData<Inputs>,
-    _out_phantom: PhantomData<Outputs>,
+    pub main: &'shaders CompiledShader<T>,
+    pub shared: Vec<&'shaders CompiledShader<T>>
 }
 
-impl<'shaders, T, I, O> ShaderStage<'shaders, T, I, O>
+impl<'s, T> ShaderStage<'s, T>
 where
     T: shader::Target,
 {
-    pub fn new(shader: &'shaders CompiledShader<T>) -> Self {
+    pub fn new(main: &'s CompiledShader<T>) -> Self {
         Self {
-            shaders: nonempty!(shader),
-            _in_phantom: PhantomData,
-            _out_phantom: PhantomData,
+            main,
+            shared: Vec::new()
         }
     }
 }
+
 
 pub trait LinkingStatus {}
 
@@ -49,7 +57,8 @@ impl LinkingStatus for UnLinked {}
 pub struct Linked;
 impl LinkingStatus for Linked {}
 
-pub struct ProgramAllocator;
+#[derive(Default)]
+struct ProgramAllocator;
 
 unsafe impl Allocator for ProgramAllocator {
     fn allocate(names: &mut [u32]) {
@@ -68,163 +77,343 @@ unsafe impl Allocator for ProgramAllocator {
     }
 }
 
-struct ProgramSemantics<'shaders, I, O, L>
+struct ProgramSemantics<I, O>
 where
-    L: LinkingStatus,
+    I: parameters::Parameters,
+    O: parameters::Parameters,
 {
-    pub _linking_status: PhantomData<L>,
-    pub vertex_stage: ShaderStage<'shaders, Vertex, I, ()>,
-    pub tesselation_stage: Option<(
-        ShaderStage<'shaders, tesselation::Control, (), ()>,
-        ShaderStage<'shaders, tesselation::Evaluation, (), ()>,
-    )>,
-    pub geometry_stage: Option<ShaderStage<'shaders, Geometry, (), ()>>,
-    pub fragment_stage: ShaderStage<'shaders, Fragment, (), O>,
-    pub compute_stage: Option<ShaderStage<'shaders, Compute, (), ()>>,
+    pub _input_phantom: PhantomData<I>,
+    pub _output_phantom: PhantomData<O>,
 }
 
-impl<'shaders, I, O> ProgramSemantics<'shaders, I, O, UnLinked> {
-    pub fn link(self) -> ProgramSemantics<'shaders, I, O, Linked> {
-        ProgramSemantics::<I, O, Linked> {
-            _linking_status: PhantomData,
-            vertex_stage: self.vertex_stage,
-            tesselation_stage: self.tesselation_stage,
-            geometry_stage: self.geometry_stage,
-            fragment_stage: self.fragment_stage,
-            compute_stage: self.compute_stage,
-        }
+impl<I: parameters::Parameters, O: parameters::Parameters> std::default::Default for ProgramSemantics<I, O> {
+    fn default() -> Self {
+        Self { _input_phantom: Default::default(), _output_phantom: Default::default() }
     }
 }
 
 #[doc = include_str!("../../../docs/object/program/Program.md")]
-pub struct Program<'shaders, I, O, L = UnLinked>
+pub struct Program<I, O>
 where
-    L: LinkingStatus,
+    I: parameters::Parameters,
+    O: parameters::Parameters,
 {
     object: Object<ProgramAllocator>,
-    semantics: ProgramSemantics<'shaders, I, O, L>,
+    semantics: ProgramSemantics<I, O>,
 }
 
-impl<'shaders, I, O> Program<'shaders, I, O, UnLinked> {
-    pub fn link(self) -> Program<'shaders, I, O, Linked> {
-        let Self { object, semantics } = self;
-        unsafe {
-            gl::LinkProgram(object.name());
-        }
-
-        Program::<'shaders, _, _, _> {
-            object,
-            semantics: semantics.link(),
-        }
-    }
-}
-
-/// Marker that specifies what types are sufficient for
-pub trait StageConfiguration {}
-impl StageConfiguration for CompiledShader<Vertex> {}
-impl StageConfiguration for CompiledShader<Fragment> {}
-impl StageConfiguration for CompiledShader<Geometry> {}
-impl StageConfiguration for CompiledShader<Compute> {}
-impl StageConfiguration for CompiledShader<tesselation::Control> {}
-impl StageConfiguration for CompiledShader<tesselation::Evaluation> {}
-impl StageConfiguration
-    for (
-        CompiledShader<tesselation::Control>,
-        CompiledShader<tesselation::Evaluation>,
-    )
-{
-}
-
-trait Attach<C>
+impl<I, O> std::default::Default for Program<I, O>
 where
-    C: StageConfiguration,
+    I: parameters::Parameters,
+    O: parameters::Parameters,
 {
-    type Config;
-
-    fn attach(&mut self, config: Self::Config);
-}
-
-impl<'shaders, I, O> Attach<CompiledShader<Vertex>> for Program<'shaders, I, O> {
-    type Config = &'shaders CompiledShader<Vertex>;
-
-    fn attach(&mut self, shader: Self::Config) {
-        self.semantics.vertex_stage.shaders.push(shader);
+    fn default() -> Self {
+        Self { object: Default::default(), semantics: Default::default() }
     }
 }
 
-impl<'shaders, I, O> Attach<CompiledShader<Fragment>> for Program<'shaders, I, O> {
-    type Config = &'shaders CompiledShader<Fragment>;
-
-    fn attach(&mut self, shader: Self::Config) {
-        self.semantics.fragment_stage.shaders.push(shader);
-    }
+#[repr(u32)]
+pub enum QueryParam {
+    DeleteStatus = gl::DELETE_STATUS,
+    LinkStatus = gl::LINK_STATUS,
+    ValidateStatus = gl::VALIDATE_STATUS,
+    InfoLogLength = gl::INFO_LOG_LENGTH,
+    AttachedShaders = gl::ATTACHED_SHADERS,
+    ActiveAtomicCounterBuffers = gl::ACTIVE_ATOMIC_COUNTER_BUFFERS,
+    ActiveAttributes = gl::ACTIVE_ATTRIBUTES,
+    ActiveAttributeMaxLength = gl::ACTIVE_ATTRIBUTE_MAX_LENGTH,
+    ActiveUniforms = gl::ACTIVE_UNIFORMS,
+    ActiveUniformBlocks = gl::ACTIVE_UNIFORM_BLOCKS,
+    ActiveUniformBlockMaxNameLength = gl::ACTIVE_UNIFORM_BLOCK_MAX_NAME_LENGTH,
+    ActiveUniformMaxLength = gl::ACTIVE_UNIFORM_MAX_LENGTH,
+    ComputeWorkGroupSize = gl::COMPUTE_WORK_GROUP_SIZE,
+    ProgramBinaryLength = gl::PROGRAM_BINARY_LENGTH,
+    TransformFeedbackBufferMode = gl::TRANSFORM_FEEDBACK_BUFFER_MODE,
+    TransformFeedbackVaryings = gl::TRANSFORM_FEEDBACK_VARYINGS,
+    TransformFeedbackVaryingMaxLength = gl::TRANSFORM_FEEDBACK_VARYING_MAX_LENGTH,
+    GeometryVerticesOut = gl::GEOMETRY_VERTICES_OUT,
+    GeometryInputType = gl::GEOMETRY_INPUT_TYPE,
+    GeometryOutputType = gl::GEOMETRY_OUTPUT_TYPE,
 }
 
-impl<'shaders, I, O> Attach<CompiledShader<Geometry>> for Program<'shaders, I, O> {
-    type Config = &'shaders CompiledShader<Geometry>;
-
-    fn attach(&mut self, shader: Self::Config) {
-        match self.semantics.geometry_stage {
-            Some(ref mut stage) => stage.shaders.push(shader),
-            None => self.semantics.geometry_stage = Some(ShaderStage::new(shader)),
-        };
-    }
-}
-
-impl<'shaders, I, O> Attach<CompiledShader<Compute>> for Program<'shaders, I, O> {
-    type Config = &'shaders CompiledShader<Compute>;
-
-    fn attach(&mut self, shader: Self::Config) {
-        match self.semantics.compute_stage {
-            Some(ref mut stage) => stage.shaders.push(shader),
-            None => self.semantics.compute_stage = Some(ShaderStage::new(shader)),
-        };
-    }
-}
-
-impl<'shaders, I, O>
-    Attach<(
-        CompiledShader<tesselation::Control>,
-        CompiledShader<tesselation::Evaluation>,
-    )> for Program<'shaders, I, O>
+impl<I, O> Program<I, O>
+where
+    I: parameters::Parameters,
+    O: parameters::Parameters,
 {
-    type Config = &'shaders (
-        CompiledShader<tesselation::Control>,
-        CompiledShader<tesselation::Evaluation>,
-    );
-
-    fn attach(&mut self, (control, evaluation): Self::Config) {
-        match self.semantics.tesselation_stage {
-            Some((ref mut old_control, ref mut old_evaluation)) => {
-                old_control.shaders.push(control);
-                old_evaluation.shaders.push(evaluation);
+    // consider intoducing no input / output types so this method is not accessible
+    pub fn builder<'s>(vertex_shader: &'s Main<Vertex, I, O>) -> Builder<'s, Vertex, I, O>
+    where
+        I: parameters::Parameters,
+        O: parameters::Parameters,
+    {
+        Builder::new(vertex_shader)
+    }
+    
+    pub fn query(&self, param: QueryParam, output: &mut i32) {
+        gl_call! {
+            #[panic]
+            unsafe {
+                gl::GetProgramiv(self.object.name(), param as _, output);
             }
-            None => {
-                self.semantics.tesselation_stage =
-                    Some((ShaderStage::new(control), ShaderStage::new(evaluation)))
+        }
+    }
+
+    pub fn info_log(&self) -> Option<String> {
+        let mut log_size = 0;
+        self.query(QueryParam::InfoLogLength, &mut log_size);
+        (log_size > 0).then(|| {
+            let mut buffer: Vec<u8> = Vec::with_capacity(log_size as _);
+            let mut actual_length = 0;
+            gl_call! {
+                #[panic]
+                // SAFETY: All values passed are valid
+                // todo: notes on error situations
+                unsafe {
+                    gl::GetProgramInfoLog(
+                        self.object.name(),
+                        buffer.capacity() as _,
+                        &mut actual_length as *mut _,
+                        buffer.as_mut_ptr() as _
+                    )
+                }
             }
-        };
+            // GetShaderInfoLog does not account for null terminator in returned length.
+            // SAFETY: nothing will panic here so it's safe to set length.
+            unsafe {
+                buffer.set_len((actual_length) as _);
+            }
+            // SAFETY: todo will shader compiler should emmit valid ascii?
+            unsafe { String::from_utf8_unchecked(buffer) }
+        })
+    }
+
+    pub(crate) fn attach<T: shader::Target>(&self, stage: &ShaderStage<T>) {
+        let main = stage.main;
+        unsafe {
+            gl::AttachShader(self.object.name(), main.object.name());
+        }
+        for shared in &stage.shared {
+            unsafe {
+                gl::AttachShader(self.object.name(), shared.object.name());
+            }
+        }
+    }
+
+    pub(self) fn link(self) -> Result<Program<I, O>, CompilationError> {
+        unsafe {
+            gl::LinkProgram(self.object.name());
+        }
+
+        self.info_log().map_or(
+            // SAFETY: we just checked if shader compiled successfully
+            Ok(unsafe { self }),
+            |msg| Err(CompilationError { msg }),
+        )
     }
 }
 
-impl<'shaders, I, O> Attach<CompiledShader<tesselation::Control>> for Program<'shaders, I, O> {
-    type Config = &'shaders CompiledShader<tesselation::Control>;
+impl<I: parameters::Parameters, O: parameters::Parameters> resource::Bindable for Program<I, O> {
+    fn bind(&self) {
+        gl_call! {
+            #[panic]
+            unsafe {
+                gl::UseProgram(self.object.name())
+            }
+        }
+    }
 
-    fn attach(&mut self, shader: Self::Config) {
-        let Some((control, _)) = self.semantics.tesselation_stage.as_mut() else {
-            panic!("tesselation stage unattached");
-        };
-        control.shaders.push(shader);
+    fn unbind(&self) {
+        gl_call! {
+            #[panic]
+            unsafe {
+                // todo: should this be the case?
+                gl::UseProgram(0)
+            }
+        }
     }
 }
 
-impl<'shaders, I, O> Attach<CompiledShader<tesselation::Evaluation>> for Program<'shaders, I, O> {
-    type Config = &'shaders CompiledShader<tesselation::Evaluation>;
 
-    fn attach(&mut self, shader: Self::Config) {
-        let Some((_, evaluation)) = self.semantics.tesselation_stage.as_mut() else {
-            panic!("tesselation stage unattached");
-        };
-        evaluation.shaders.push(shader);
+
+pub struct Builder<'shaders, T, I, O>
+where
+    T: shader::Target,
+    I: parameters::Parameters,
+    O: parameters::Parameters,
+{
+    _target_phantom: PhantomData<T>,
+    _input_phantom: PhantomData<I>,
+    _output_phantom: PhantomData<O>,
+    vertex: ShaderStage<'shaders, Vertex>,
+
+    // todo: It would be nice to implement type state here to avoid options
+    tesselation_control: Option<ShaderStage<'shaders, tesselation::Control>>,
+    // Attach relation assures correctness
+    tesselation_evaluation: Option<ShaderStage<'shaders, tesselation::Evaluation>>,
+    geometry: Option<ShaderStage<'shaders, Geometry>>,
+    fragment: Option<ShaderStage<'shaders, Fragment>>,
+    compute: Option<ShaderStage<'shaders, Compute>>,
+}
+
+
+impl<'s, T, I, O> Builder<'s, T, I, O>
+where
+    T: shader::Target,
+    I: parameters::Parameters,
+    O: parameters::Parameters,
+{
+    fn retype<NT: shader::Target, NO: parameters::Parameters>(self) -> Builder<'s, NT, I, NO> {
+        Builder {
+            _output_phantom: PhantomData,
+            _target_phantom: PhantomData,
+            _input_phantom: PhantomData,
+            vertex: self.vertex,
+            tesselation_control: self.tesselation_control,
+            tesselation_evaluation: self.tesselation_evaluation,
+            geometry: self.geometry,
+            fragment: self.fragment,
+            compute: self.compute,
+        }
     }
 }
+
+impl<'s, I, O> Builder<'s, Vertex, I, O>
+where
+    I: parameters::Parameters,
+    O: parameters::Parameters,
+{
+    pub fn new(vertex_shader: &'s Main<Vertex, I, O>) -> Self {
+        Self {
+            _target_phantom: PhantomData,
+            _input_phantom: PhantomData,
+            _output_phantom: PhantomData,
+            vertex: ShaderStage::new(vertex_shader),
+            tesselation_control: None,
+            tesselation_evaluation: None,
+            geometry: None,
+            fragment: None,
+            compute: None,
+        }
+    }
+
+    pub fn vertex_shared(mut self, shader: &'s Shared<Vertex>) -> Self {    
+        self.vertex.shared.push(shader);
+        self
+    }
+
+    pub fn tesselation_control_main<TCO: parameters::Parameters>(mut self, shader: &'s Main<tesselation::Control, O, TCO>) -> Builder<tesselation::Control, I, TCO> {    
+        self.tesselation_control = Some(ShaderStage::new(shader));
+        self.retype()
+    }
+
+    pub fn tesselation_evaluation_main<TEO: parameters::Parameters>(mut self, shader: &'s Main<tesselation::Evaluation, O, TEO>) -> Builder<tesselation::Control, I, TEO> {    
+        self.tesselation_evaluation = Some(ShaderStage::new(shader));
+        self.retype()
+    }
+
+    pub fn geometry_main<GO: parameters::Parameters>(mut self, geometry: &'s Main<Geometry, O, GO>) -> Builder<Geometry, I, GO> {
+        self.geometry = Some(ShaderStage::new(geometry));
+        self.retype()
+    }
+
+    pub fn fragment_main<FO: parameters::Parameters>(mut self, fragment: &'s Main<Fragment, O, FO>) -> Builder<Fragment, I, FO> {
+        self.fragment = Some(ShaderStage::new(fragment));
+        self.retype()
+    }
+}
+
+impl<'s, I, O> Builder<'s, tesselation::Control, I, O>
+where
+    I: parameters::Parameters,
+    O: parameters::Parameters,
+{
+    pub fn tesselation_control_shared(mut self, shared: &'s Shared<tesselation::Control>) -> Self {
+        self.tesselation_control.as_mut().expect("tesselation control was initialized").shared.push(shared);
+        self
+    }
+
+    pub fn tesselation_evaluation_main<TEO>(mut self, shader: &'s Main<tesselation::Evaluation, O, TEO>) -> Builder<tesselation::Evaluation, I, TEO>
+    where
+        TEO: parameters::Parameters,
+    {    
+        self.tesselation_evaluation = Some(ShaderStage::new(shader));
+        self.retype()
+    }
+}
+
+impl<'s, I, O> Builder<'s, tesselation::Evaluation, I, O>
+where
+    I: parameters::Parameters,
+    O: parameters::Parameters,
+{
+    pub fn tesselation_evaluation_shared(mut self, shared: &'s Shared<tesselation::Evaluation>) -> Self {
+        self.tesselation_evaluation.as_mut().expect("tesselation evaluation stage was initialized").shared.push(shared);
+        self
+    }
+
+    pub fn geometry_main<GO: parameters::Parameters>(mut self, geometry: &'s Main<Geometry, O, GO>) -> Builder<Geometry, I, GO> {
+        self.geometry = Some(ShaderStage::new(geometry));
+        self.retype()
+    }
+
+    pub fn fragment_main<FO: parameters::Parameters>(mut self, fragment: &'s Main<Fragment, O, FO>) -> Builder<Fragment, I, FO> {
+        self.fragment = Some(ShaderStage::new(fragment));
+        self.retype()
+    }
+}
+
+impl<'s, I, O> Builder<'s, Geometry, I, O>
+where
+    I: parameters::Parameters,
+    O: parameters::Parameters,
+{
+    pub fn geometry_shared(mut self, shared: &'s Shared<Geometry>) -> Self {
+        self.geometry.as_mut().expect("geometry stage was initialized").shared.push(shared);
+        self
+    }
+
+    pub fn fragment_main<FO: parameters::Parameters>(mut self, fragment: &'s Main<Fragment, O, FO>) -> Builder<Fragment, I, FO> {
+        self.fragment = Some(ShaderStage::new(fragment));
+        self.retype()
+    }
+}
+
+impl<'s, I, O> Builder<'s, Fragment, I, O>
+where
+    I: parameters::Parameters,
+    O: parameters::Parameters,
+{
+    pub fn fragment_shared(mut self, shared: &'s Shared<Fragment>) -> Self {
+        self.fragment.as_mut().expect("fragment stage was initialized").shared.push(shared);
+        self
+    }
+
+    pub fn build(&self) -> Result<Program<I, O>, CompilationError> {
+        let program = Program::default();
+        program.attach(&self.vertex);
+
+        if let (Some(control_stage), Some(evaluation_stage)) = (&self.tesselation_control, &self.tesselation_evaluation) {
+            program.attach(control_stage);
+            program.attach(evaluation_stage);
+        }
+
+        if let Some(geometry) = &self.geometry {
+            program.attach(geometry);
+        }
+
+        if let Some(fragment) = &self.geometry {
+            program.attach(fragment);
+        }
+
+        program.link()
+    }
+}
+
+impl<'s, I, O> Builder<'s, Vertex, I, O>
+where
+    I: parameters::Parameters,
+    O: parameters::Parameters,
+{
+    
+} 
