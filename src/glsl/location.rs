8f2d7ee02cf08dfead0d471db84e152423b5427e
Mikołaj Depta <328690@uwr.edu.pl>
2024-06-24 19:02:59 +0200

    Refactor location module

--- src/glsl/location.rs
+++ src/glsl/location.rs
@@ -1,96 +1,80 @@
 //! This module provides `location` glsl attribute calculations for glsl types.
 
-use crate::{constraint, glsl};
-use glsl::{Const, Type};
+use crate::glsl;
+use glsl::Const;
+use sealed::sealed;
+use crate::valid;
 
-use self::marker::Location;
+/// If anyone thinks this should be unsafe come and fight me!
+#[sealed]
+pub trait Location {
+    const LOCATION_COUNT: usize;
+}
 
-use super::marker::{Matrix, MatrixType, Vector};
-
-pub mod marker {
-    pub unsafe trait Location {
-        const LOCATION_COUNT: usize;
+macro_rules! impl_location {
+    ($ty: ty: $location: literal) => {
+        #[sealed]
+        impl Location for $ty {
+            const LOCATION_COUNT: usize = $location;
+        }
     }
 }
 
-unsafe impl marker::Location for f32 {
-    const LOCATION_COUNT: usize = 1;
-}
-
-unsafe impl marker::Location for f64 {
-    const LOCATION_COUNT: usize = 1;
-}
-
-unsafe impl marker::Location for i32 {
-    const LOCATION_COUNT: usize = 1;
-}
-
-unsafe impl marker::Location for u32 {
-    const LOCATION_COUNT: usize = 1;
-}
-
-unsafe impl marker::Location for bool {
-    const LOCATION_COUNT: usize = 1;
-}
+impl_location!{ f32: 1 }
+impl_location!{ f64: 1 }
+impl_location!{ i32: 1 }
+impl_location!{ u32: 1 }
 
 /// Location count for glsl vecX types.
-unsafe impl<const VEC_SIZE: usize> marker::Location for glsl::Vec<VEC_SIZE>
+impl<const VEC_SIZE: usize> Location for glsl::Vec<VEC_SIZE>
 where
-    Const<VEC_SIZE>: constraint::Valid<Vector>,
+    Const<VEC_SIZE>: valid::ForVector,
 {
     const LOCATION_COUNT: usize = 1;
 }
 
 /// Location count for glsl ivecX types.
-unsafe impl<const VEC_SIZE: usize> marker::Location for glsl::IVec<VEC_SIZE>
+impl<const VEC_SIZE: usize> Location for glsl::IVec<VEC_SIZE>
 where
-    Const<VEC_SIZE>: constraint::Valid<Vector>,
+    Const<VEC_SIZE>: valid::ForVector,
 {
     const LOCATION_COUNT: usize = 1;
 }
 
 /// Location count for glsl uvecX types.
-unsafe impl<const VEC_SIZE: usize> marker::Location for glsl::UVec<VEC_SIZE>
+impl<const VEC_SIZE: usize> Location for glsl::UVec<VEC_SIZE>
 where
-    Const<VEC_SIZE>: constraint::Valid<Vector>,
+    Const<VEC_SIZE>: valid::ForVector,
 {
     const LOCATION_COUNT: usize = 1;
 }
 
 /// Location count for glsl dvecX types is different dvec2 take 1 location and dvec3/4 take 2.
-unsafe impl<const VEC_SIZE: usize> marker::Location for glsl::DVec<VEC_SIZE>
+impl<const VEC_SIZE: usize> Location for glsl::DVec<VEC_SIZE>
 where
-    Const<VEC_SIZE>: constraint::Valid<Vector>,
+    Const<VEC_SIZE>: valid::ForVector,
 {
     const LOCATION_COUNT: usize = match VEC_SIZE {
         2 => 1,
         3 | 4 => 2,
-        _ => unreachable!(), // VecSize bound should prevent this code from being ever reached
+        _ => unreachable!(""), // `Const<VEC_SIZE>: valid::ForVector` should prevent this code from being ever reached
     };
 }
 
-/// Location count for glsl bvecX types.
-// unsafe impl<const VEC_SIZE: usize> marker::Location for glsl::BVec<VEC_SIZE>
-// where
-//     Const<VEC_SIZE>: constraint::Valid<Vector>,
-// {
-//     const LOCATION_COUNT: usize = 1;
-// }
-
 /// Location for an Array of `T` where `T: Location` of size `N` is `N * <T as Location>::LOCATION_COUNT`,
-unsafe impl<T, const N: usize> marker::Location for glsl::Array<T, N>
+impl<T, const N: usize> Location for glsl::Array<T, N>
 where
-    T: Type,
+    T: glsl::Type,
 {
     const LOCATION_COUNT: usize = T::LOCATION_COUNT * N;
 }
 
 /// Accordingly to GLSL spec matrices use the same number of locations as arrays of Row
-unsafe impl<T, const ROW_SIZE: usize, const COL_SIZE: usize> marker::Location for glsl::Mat<T, ROW_SIZE, COL_SIZE>
+impl<T, const ROW_SIZE: usize, const COL_SIZE: usize> Location for glsl::Mat<T, ROW_SIZE, COL_SIZE>
 where
-    T: glsl::Type + constraint::Valid<Matrix>,
-    Const<ROW_SIZE>: constraint::Valid<Vector>,
-    Const<COL_SIZE>: constraint::Valid<Vector>,
+    T: glsl::Type + valid::ForMatrix,
+    Const<ROW_SIZE>: valid::ForVector,
+    Const<COL_SIZE>: valid::ForVector,
 {
-    const LOCATION_COUNT: usize = <glsl::Array<glsl::base::Vec<T, COL_SIZE>, ROW_SIZE> as marker::Location>::LOCATION_COUNT;
+    const LOCATION_COUNT: usize = <glsl::Array<glsl::GVec<T, COL_SIZE>, ROW_SIZE> as Location>::LOCATION_COUNT;
 }
