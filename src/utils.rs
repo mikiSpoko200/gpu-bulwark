1b552e2e8f085ece4206f8c841f5618654676ee7
Mikołaj Depta <328690@uwr.edu.pl>
2024-08-02 17:31:25 +0200

    Add draft for const indexing

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use crate::hlist::{self, lhlist};
+
 
 pub trait Const<T> {
     const VALUE: T;
@@ -27,4 +29,29 @@ macro_rules! disjoint {
             )+
         )+
     };
+}
+
+pub trait ConstIndexMut<I: hlist::counters::Index> {
+    type Output<const N: usize>;
+
+    fn const_index_mut<const INDEX: usize>(&self) -> Self::Output<INDEX>
+    where
+        Self: lhlist::Find<Self::Output<INDEX>, I>
+    ;
+}
+
+pub trait ConstIndex<T> {
+    type Output<const N: usize>;
+
+    fn const_index<const INDEX: usize>(&self) -> Self::Output<INDEX>;
+}
+
+/// Maybe const eval allows some magic to happen with slices? then we could loosen the requirement for locations to be increasing.
+/// FIXME: Besides increasing sequences would be painful to maintain if they were to be mutable - as in allow insertion and deletion different that pop / append
+fn assert_const_indexable(lhs: usize, rhs: usize) {
+    if !(lhs < rhs) { panic!("invalid types, lhs < rhs does not hold") }
+}
+
+pub trait ConstIndexHList<I> {
+
 }
