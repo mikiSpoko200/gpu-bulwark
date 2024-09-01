5f7d9a548fd4b9d5926ded38f63b0c84be464b44
mikiSpoko200 <328690@uwr.edu.pl>
2024-09-01 20:32:14 +0200

    listing-5

--- presentation/shaders/vert.glsl
+++ presentation/shaders/vert.glsl
@@ -1,11 +1,11 @@
 #version 420 core
 
-layout(location = 0) in vec3 in_color;
+layout(location = 0) in ivec3 in_color;
 layout(location = 1) in vec3 in_position;
 
 layout(location = 0) out vec4 color;
 
 void main() {
     gl_Position = vec4(in_position, 1.0);
-    color = vec4(in_color, 1.0);
+    color = vec4(vec3(in_color / 16), 1.0);
 }
