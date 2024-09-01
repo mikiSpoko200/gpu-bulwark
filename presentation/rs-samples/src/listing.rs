f09127b69ea4eb0c1bdb6b865556210da53ea784
mikiSpoko200 <328690@uwr.edu.pl>
2024-09-01 20:32:14 +0200

    listing-6

--- presentation/rs-samples/src/listing.rs
+++ presentation/rs-samples/src/listing.rs
@@ -15,7 +15,7 @@ use gl::buffer::{Dynamic, Draw};
 use gl::{Program, Buffer, VertexArray};
 
 type VsInputs = glsl::Glsl! {
-    layout(location = 0) in vec3;
+    layout(location = 0) in ivec3;
     layout(location = 1) in vec3;
 };
 
@@ -32,7 +32,7 @@ type FsOutputs = glsl::Glsl! {
 };
 
 type Attributes = gb::HList! {
-    Attribute<[f32; 3], 0>,
+    Attribute<[i32; 3], 0>,
     Attribute<[f32; 3], 1>,
 };
 
@@ -86,10 +86,11 @@ impl crate::Sample for Listing {
         let mut colors = Buffer::create();
         let mut positions = Buffer::create();
 
+        // NOTE: Invalid data format
         colors.data::<(Dynamic, Draw)>(&[
             [1.0, 0.0, 0.0], 
             [0.0, 1.0, 0.0], 
-            [0.0, 0.0, 1.032]
+            [0.0, 0.0, 1.0f32]
         ]);
         positions.data::<(Dynamic, Draw)>(&[
             [-0.5, -0.5, -1.0], 
