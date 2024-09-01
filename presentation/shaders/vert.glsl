905a85b3e59cd135d28a4d9bbe8c2ff7e792a811
mikiSpoko200 <328690@uwr.edu.pl>
2024-09-01 19:42:58 +0200

    Uniforms listing

--- presentation/shaders/vert.glsl
+++ presentation/shaders/vert.glsl
@@ -1,11 +1,17 @@
-#version 420 core
+#version 460 core
 
-layout(location = 0) in vec3 in_color;
-layout(location = 1) in vec3 in_position;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec4 color;
 
-layout(location = 0) out vec4 color;
+layout(location = 0) uniform float multiplier;
+layout(location = 1) uniform float x_offset;
+layout(location = 2) uniform float y_offset;
+
+layout(location = 0) out vec4 vertexColor;
 
 void main() {
-    gl_Position = vec4(in_position, 1.0);
-    color = vec4(in_color, 1.0);
+    gl_Position = vec4(position, 1.0);
+    gl_Position.x += x_offset;
+    gl_Position.y += y_offset;
+    vertexColor = color * multiplier;
 }
