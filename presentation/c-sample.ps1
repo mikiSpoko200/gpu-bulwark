1a120f6bb14eead9f9d8b7cfb83afa5c5aab20fe
Mikołaj Depta <328690@uwr.edu.pl>
2024-11-28 22:37:55 +0100

    remove presentation

--- presentation/c-sample.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Building C sample . . ."
-msbuild c-samples/c-samples.sln /p:Configuration=Release /p:Platform=x64 /nologo /verbosity:quiet
-
-Read-Host -Prompt "Press Enter run C sample . . ."
-.\c-samples\x64\Release\c-samples.exe
--- presentation/rs-sample.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Building Rust sample . . ."
-cargo b --manifest-path "./rs-samples/Cargo.toml" --quiet
-Read-Host -Prompt "Press Enter run Rust sample . . ."
-cargo r --manifest-path "./rs-samples/Cargo.toml" --quiet
