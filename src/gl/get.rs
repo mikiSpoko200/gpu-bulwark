b401a263529977e74b20c1ab18ca06a20ec085e8
mikiSpoko200 <328690@uwr.edu.pl>
2025-01-19 21:08:08 +0100

    feat: finalize get api

--- src/gl/get.rs
+++ src/gl/get.rs
@@ -3,50 +3,78 @@
 use crate::gl;
 
 /// Wrapper around OpenGL literals.
-pub struct SymbolicConstant<const SYMBOLIC_CONSTANT: glb::types::GLenum>;
+pub struct SymbolicConstant<const VALUE: glb::types::GLenum>;
 
-pub trait Meta {
-    type Get: Get;
+mod sealed {
+    pub trait GetSymbolicConstant {
+        type Value: Get;
+        fn get() -> Self::Value;
+    }
+
+    pub trait Get: Default + Sized {
+        fn get(pname: glb::types::GLenum) -> Self;
+    }
 }
 
-pub trait Get: Default + Sized {
-    fn get(&mut self, pname: glb::types::GLenum) -> Self;
-}
+pub use sealed::*;
 
-impl Get for i32 {
-    fn get(&mut self, pname: glb::types::GLenum) -> Self {
-        gl::call! {
-            [panic]
-            unsafe {
-                glb::GetIntegerv(pname, self as *mut _);
+macro_rules! impl_get {
+    ($ty:ty, $proc:ident) => {
+        impl Get for $ty {
+            fn get(pname: glb::types::GLenum) -> Self {
+                let mut ret = Default::default();
+                unsafe {
+                    glb::$proc(pname, &raw mut ret);
+                }
+                ret
             }
         }
-        *self
+    };
+}
+
+impl_get! { i32, GetIntegerv }
+impl_get! { f32, GetFloatv }
+impl_get! { f64, GetDoublev }
+impl_get! { i64, GetInteger64v }
+
+// Boolean in special, we convert to rust bool
+impl Get for bool {
+    fn get(pname: glb::types::GLenum) -> Self {
+        let mut gl_bool = Default::default();
+        unsafe {
+            glb::GetBooleanv(pname, &raw mut gl_bool);
+        }
+        gl_bool == glb::TRUE
     }
 }
 
 macro_rules! symbolic_constant {
-    ($($gl_symbolic_constants:ident use $getter:ty),+ $(,)?) => {
+    ($getter:ty => [$($gl_symbolic_constants:ident),+ $(,)?]) => {
         $(
-            symbolic_constant!(@constify $gl_symbolic_constants)
-            impl Meta for SymbolicConstant<::glb::$gl_symbolic_constants> {
-                type Get = $getter;
+            pub const $gl_symbolic_constants: ::glb::types::GLenum = ::glb::$gl_symbolic_constants;
+            impl GetSymbolicConstant for SymbolicConstant<{::glb::$gl_symbolic_constants}> {
+                type Value = $getter;
+                fn get() -> Self::Value {
+                    <Self::Value as Get>::get(::glb::$gl_symbolic_constants)
+                }
             }
         )+
     };
-
-    (@constify $gl_symbolic_constants:ident) => {
-        impl SymbolicConstant<::glb::$gl_symbolic_constants> {
-            const $gl_symbolic_constants = Self(::glb::$gl_symbolic_constants);
-        }
-    };
 }
 
-pub fn get<const SYMBOLIC_CONSTANT: glb::types::GLenum>() -> <SymbolicConstant<SYMBOLIC_CONSTANT> as Meta>::Get
+// NOTE: exposing raw consntats is quite bad, would much rather have them as wrapped into [SymbolicConstant] for type safety
+//  but then it could create problems with custom const types which is needed for smeamless reimports of identifiers.
+
+symbolic_constant! {
+    i32 => [
+        ELEMENT_ARRAY_BUFFER_BINDING,
+        ARRAY_BUFFER_BINDING
+    ]
+}
+
+pub fn get<const VALUE: glb::types::GLenum>() -> <SymbolicConstant<VALUE> as GetSymbolicConstant>::Value
 where
-    SymbolicConstant<SYMBOLIC_CONSTANT>: Meta
+    SymbolicConstant<VALUE>: GetSymbolicConstant
 {
-    let mut ret = <SymbolicConstant<SYMBOLIC_CONSTANT> as Meta>::Get::default();
-    ret.get(SYMBOLIC_CONSTANT);
-    ret
+    SymbolicConstant::<VALUE>::get()
 }
